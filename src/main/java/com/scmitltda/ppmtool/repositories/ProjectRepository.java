39862cc6a08ce42e5fae08fd02a1ac3cd73eb973
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-03-21 16:28:08 -0300

    9.Project Object & Project Repository

---
+++ src/main/java/com/scmitltda/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.scmitltda.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scmitltda.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> iterable);
+}
