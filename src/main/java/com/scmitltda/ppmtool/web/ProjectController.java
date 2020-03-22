f636886e4acf93d113cfada1b6dea69561ba6daa
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-03-22 17:38:46 -0300

    14. Refactor Project Controller

--- src/main/java/com/scmitltda/ppmtool/web/ProjectController.java
+++ src/main/java/com/scmitltda/ppmtool/web/ProjectController.java
@@ -1,7 +1,6 @@
 package com.scmitltda.ppmtool.web;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scmitltda.ppmtool.domain.Project;
+import com.scmitltda.ppmtool.services.MapValidationErrorService;
 import com.scmitltda.ppmtool.services.ProjectService;
 
 @RestController
@@ -26,19 +26,14 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 		
-		if (result.hasErrors()) {
-			
-			Map<String, String> errorMap = new HashMap<>();
-			
-			for (FieldError error : result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-			
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+		if (errorMap != null) return errorMap;
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
