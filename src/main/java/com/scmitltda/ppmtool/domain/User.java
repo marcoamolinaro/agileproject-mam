26336e8ec3f94fa0211eef31226daebf7d28276a
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-05-20 22:08:36 -0300

    81. Create User Object, Validation, Repository, Service

--- src/main/java/com/scmitltda/ppmtool/domain/User.java
+++ src/main/java/com/scmitltda/ppmtool/domain/User.java
@@ -1,5 +1,110 @@
 package com.scmitltda.ppmtool.domain;
 
-public class User {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Email(message="Username needs to be an email")
+	@NotBlank(message = "Username is required")
+	@Column(unique = true)
+	private String username;
+	
+	@NotBlank(message = "Please enter your full name")
+	private String fullName;
+	
+	@NotBlank(message = "Password is required")
+	private String password;
+	
+	@Transient
+	private String ConfirmedPassword;
+	
+	private Date createdAt;
+	
+	private Date updatedAt;
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+	
+	// OneToMany with Project
+
+	public User() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmedPassword() {
+		return ConfirmedPassword;
+	}
+
+	public void setConfirmedPassword(String confirmedPassword) {
+		ConfirmedPassword = confirmedPassword;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}	
 }
---
+++ src/main/java/com/scmitltda/ppmtool/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.scmitltda.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.scmitltda.ppmtool.domain.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+	
+}
