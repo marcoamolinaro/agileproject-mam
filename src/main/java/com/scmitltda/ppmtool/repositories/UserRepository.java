7a185fb5f66e5d7fe2bfae03df340646dd7d46a5
Marco Molinaro <molinaromarcoaurelio@gmail.com>
2020-06-01 21:13:50 -0300

    85. JWT Provider pre-work

--- src/main/java/com/scmitltda/ppmtool/repositories/UserRepository.java
+++ src/main/java/com/scmitltda/ppmtool/repositories/UserRepository.java
@@ -8,4 +8,7 @@ import com.scmitltda.ppmtool.domain.User;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
 	
+	User findByUsername(String username);
+	
+	User getById(Long Id);
 }
---
+++ src/main/java/com/scmitltda/ppmtool/services/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.scmitltda.ppmtool.services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.scmitltda.ppmtool.domain.User;
+import com.scmitltda.ppmtool.repositories.UserRepository;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		if (user == null) new UsernameNotFoundException("User not found");		
+		return user;
+	}
+	
+	@Transactional
+	public User loadUserById(Long id) {
+		User user = userRepository.getById(id);
+		if (user == null) new UsernameNotFoundException("User not found");
+		return user;
+
+	}
+}
